63d61442adba0137bee79b194657a25105169a0b
Christian Eiselt <dev@eiselt.ch>
2019-11-15 13:35:12 +0100

    upload 2018-01-partA.js

---
+++ 2018/01/partA.pl
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+
+sub getFileContent
+{
+  my $file = shift;
+  my @output;
+
+  open(my $fh, '<:encoding(UTF-8)', $file)
+    or die "Could not open file '$file' $!";
+ 
+  while (my $row = <$fh>)
+  {
+    chomp $row;
+    $row =~ s/^\+//;
+    my $number = $row;
+	push(@output, $number);
+  }
+  
+  close($fh);
+
+  return \@output;
+}
+
+sub getResultingFrequency
+{
+  my $refInput = shift;
+  my @input = @{$refInput};
+  
+  my $result = 0;
+  
+  foreach (@input){
+	$result += $_;
+  }
+
+  return $result;
+}
+
+print(
+	getResultingFrequency(
+	  getFileContent(
+	    "input.txt"
+      )
+    )
+);
