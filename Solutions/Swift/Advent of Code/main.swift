246ea8c2b34b021ab24cb0faf723eb1bad0615c7
Christian Eiselt <dev@eiselt.ch>
2019-12-01 11:36:09 +0100

    update

---
+++ Solutions/Swift/Advent of Code/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent of Code
+//
+//  Created by Christian Eiselt on 24.11.19.
+//  Copyright © 2019 Christian Eiselt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
