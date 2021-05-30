bf33f639b4c310fa79fea9d9645d24d467e45acb
christianeiselt <dev@eiselt.ch>
2021-05-30 16:41:43 +0200

    Update Day02.java
    Closes #26

--- Java/2019/Day02/Day02.java
+++ Java/2019/Day02/Day02.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.sql.Array;
 import java.util.ArrayList;
