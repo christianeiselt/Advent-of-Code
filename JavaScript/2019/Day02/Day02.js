e5e15612f14a23fe3202b2039d142b85a5278e79
christianeiselt <githubce@eiselt.ch>
2021-09-09 21:40:12 +0200

    style(js): change to double quotes
    Closes #53

--- JavaScript/2019/Day02/Day02.js
+++ JavaScript/2019/Day02/Day02.js
@@ -2,7 +2,7 @@
 
 // Convert input.txt in array
 function inputToArray(input) {
-    const fs = require('fs');
+    const fs = require("fs");
     let inputArr = fs.readFileSync(input).toString().split('\n');
     if (inputArr[inputArr.length - 1] == '') {
         inputArr.pop();
