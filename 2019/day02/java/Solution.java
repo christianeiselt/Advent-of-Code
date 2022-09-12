2ae4ad1dcee4fadd42a862af9884c30a31422b9d
Christian Eiselt <dev@eiselt.ch>
2022-09-12 07:08:46 +0200

    fix #97

--- 2019/day02/java/Solution.java
+++ 2019/day02/java/Solution.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-public class solution {
+public class Solution {
 
 	private static final String INPUT_TXT = "input.txt";
 
