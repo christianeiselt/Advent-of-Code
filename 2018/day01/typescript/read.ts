3cef0c0d64250675b6824a422519d27d88a3947c
Christian Eiselt <dev@eiselt.ch>
2022-10-01 22:48:16 +0200

    remove unneeded file

--- 2018/day01/typescript/read.ts
+++
@@ -1,8 +0,0 @@
-/* import { readFileSync } from 'fs';
-
-const file = readFileSync('./input.txt', 'utf-8');
-console.log(file); */
-
-
-const text = await Deno.readTextFile('./input.txt');
-console.log(text);
