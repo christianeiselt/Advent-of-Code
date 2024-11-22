ed2c9199205b86084c9691982c3e8dd46b31e154
christianeiselt <githubce@eiselt.ch>
2024-11-22 20:17:50 +0100

    fix java issues

--- 2018/day01/java/Day01.java
+++ 2018/day01/java/Day01.java
@@ -1,19 +1,37 @@
-// https://adventofcode.com/2018/day/1
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+ * Solution for Advent of Code 2018, Day 1.
+ * <p>
+ * This utility class processes a list of frequency changes and calculates:
+ * 1. The resulting frequency after applying all changes.
+ * 2. The first frequency reached twice during a repeated application of changes.
+ * </p>
+ */
 public class Day01 {
 
+    /**
+     * Path to the input file containing frequency changes.
+     */
     private static final String INPUT_TXT = "input.txt";
 
+    /**
+     * Private constructor to prevent instantiation of the utility class.
+     */
     private Day01() {
         throw new UnsupportedOperationException("Utility class should not be instantiated.");
     }
 
+    /**
+     * Reads the content of a file line by line into an ArrayList.
+     *
+     * @param path the file path to read from.
+     * @return a list of strings, each representing a line from the file.
+     */
     public static ArrayList<String> getContent(final String path) {
         BufferedReader reader = null;
         final ArrayList<String> lines = new ArrayList<>();
@@ -41,6 +59,15 @@ public class Day01 {
         return lines;
     }
 
+    /**
+     * Processes frequency changes from a file and calculates:
+     * <ul>
+     * <li>The resulting frequency after applying all changes once.</li>
+     * <li>The first frequency that is reached twice during repeated application of changes.</li>
+     * </ul>
+     *
+     * @param path the file path containing frequency changes as lines of integers.
+     */
     public static void solve(final String path) {
         final ArrayList<String> changes = getContent(path);
 
@@ -75,6 +102,12 @@ public class Day01 {
         System.out.println("The first frequency reached twice is: " + duplicateFrequency);
     }
 
+    /**
+     * The main method to run the solution. It reads frequency changes from {@link #INPUT_TXT}.
+     *
+     * @param args command-line arguments (not used).
+     * @throws IOException if an I/O error occurs while reading the file.
+     */
     public static void main(final String[] args) throws IOException {
         solve(INPUT_TXT);
     }
