0e9051a4262c36efbf5e16d1cddecb4b80f42145
christianeiselt <githubce@eiselt.ch>
2024-11-24 00:11:01 +0100

    refactor(csharp):fix linter warnings

--- csharp/AdventOfCode/src/AoCShared/InputHelper.cs
+++ csharp/AdventOfCode/src/AoCShared/InputHelper.cs
@@ -2,13 +2,13 @@
 
 using System.IO;
 
-public class InputHelper
+public static class InputHelper
 {
     private const string InputsPath = "../../../../../../../_inputs";
     
     public static string GetInput(int year, int day)
     {
-        string filePath = $"{InputsPath}/{year}/day{day:D2}.txt";
+        var filePath = $"{InputsPath}/{year}/day{day:D2}.txt";
         if (File.Exists(filePath)) return Path.GetFullPath(filePath);
 
         throw new FileNotFoundException($"Input file for {year} day {day} not found.");
